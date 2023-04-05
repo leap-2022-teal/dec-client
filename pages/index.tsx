d4345ef1edd5717498a9527c3a3e2c979921c189
evening-2 <evening-2@1-9.local>
2023-04-05 15:29:57 +0800

    u

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Hello NextJS CLIENT DEC Team project</div>;
+  return (
+    <div style={{ color: "red" }}>Hello NextJS CLIENT DEC Team project</div>
+  );
 }
