8a8fe62363ce6be85b818ccfc451f50cbfed61c6
Bayarkhuu13 <bayarkhuu903@gmail.com>
2023-04-17 17:59:56 +0800

    cilent: SignIn

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -13,7 +13,7 @@ export default function Signin(){
         <section className="w-3/4 mx-auto flex flex-col gap-10">
         <div className="title">
             <h1 className="text-gray-800 text-4xl font-bold py-4">Explore</h1>
-            <p className="w-3/4 mx-auto text-gray-400">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p className="w-3/4 mx-auto text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
 
         <form className="flex flex-col gap-5">
