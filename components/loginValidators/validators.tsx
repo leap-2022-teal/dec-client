8305d220744dfb03d93aacae5d2d92d3f383e5c7
Bayarkhuu13 <bayarkhuu903@gmail.com>
2023-04-20 18:35:58 +0800

    sign in zasalt saijruulalt

---
+++ components/loginValidators/validators.tsx
@@ -0,0 +1,27 @@
+
+export  function emailValidator({email}:any){
+    console.log(email)
+    if(!email){
+        return "Emailee burtguulne uu"
+    } else if(!new RegExp(/\S+@\S+\.\S+/).test(email)){
+        return "Email bish bn"
+    } return "Success"
+}
+
+export  function passwordValidator({password}:any){
+    if(!password){
+        return "Nuuts uge orulnu"
+    } else if(password.length < 8){
+        return "nuuts ug 8 oron oos deesh baih estoi"
+    } return ""
+}
+
+export  function confirmPasswordValidator({password,cpassword}:any){
+    if(!cpassword){
+        return "nuuts ugee battganu"
+    } else if(cpassword.length < 8) {
+        return "nuuts ug 8 oron oos deesh baih estoi"
+    } else if (cpassword !== password){
+        return "nuuts ug hoorondo taarahgui bn"
+    } return "nuuts ug batalgaajla"
+}
