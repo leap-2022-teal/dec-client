0bffc4abe83477f9eb22375391d6bbc1a54d1f5d
Bayarkhuu13 <bayarkhuu903@gmail.com>
2023-04-28 16:21:35 +0800

    login backend ees shalgaj oroh

--- components/loginValidators/validators.tsx
+++ components/loginValidators/validators.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export function emailValidator({ email }: any) {
   if (!email) {
     return "Emailee burtguulne uu";
@@ -26,3 +28,73 @@ export function confirmPasswordValidator({ password, cpassword }: any) {
   }
   return "nuuts ug batalgaajla";
 }
+
+// function PasswordAndConfirmPasswordValidation() {
+//   const [passwordError, setPasswordError] = useState("");
+//   const [confirmPasswrodError, setConfirmPasswordError] = useState("");
+//   const [passwordInput, setPasswordInput] = useState({
+//     password: "",
+//     confirmPassword: "",
+//   });
+
+//   const handlePasswordChange = (evnt : any) =>{
+
+//     const passwordInputValue = evnt.target.value.trim();
+//     const passwordInputFieldName = evnt.target.name;
+//     const NewPassWordInput = {...passwordInput, [passwordInputFieldName]:passwordInputValue} setPasswordInput(NewPassWordInput);
+//   }
+
+//   const handleValidation = (evnt : any) => {
+
+//     const passwordInputValue = evnt.target.value.trim()
+//     const passwordInputFieldName = evnt.target.name
+//   }
+
+//   if(passwordInputFieldName === "password"){
+//     const uppercaseRegExp   = /(?=.*?[A-Z])/;
+//     const lowercaseRegExp   = /(?=.*?[a-z])/;
+//     const digitsRegExp      = /(?=.*?[0-9])/;
+//     const specialCharRegExp = /(?=.*?[#?!@$%^&*-])/;
+//     const minLengthRegExp   = /.{8,}/;
+
+//     const passwordLenght = passwordInputValue.length;
+//     const uppercasePassword = uppercaseRegExp.test(passwordInputValue);
+//     const lowercasePassword = lowercaseRegExp.test(passwordInputValue);
+//     const digitsPassword = digitsRegExp.test(passwordInputValue);
+//     const specialCharPassword = specialCharRegExp.test(passwordInputValue);
+//     const minLengthPassword = minLengthRegExp.test(passwordInputValue);
+
+//     let errMsg = "";
+//     if(passwordLenght === 0){
+//       errMsg="Password is empty";
+//     } else if(!uppercasePassword){
+//       errMsg="At least one Uppercase"
+//     } else if(!lowercasePassword){
+//       errMsg="At least one Lowercase"
+//     } else if(!digitsPassword){
+//       errMsg="At least one digits"
+//     } else if(!specialCharPassword){
+//       errMsg="At least one Special Characters"
+//     } else if(!minLengthPassword){
+//       errMsg="At least minumum 8 characters"
+//     } else {
+//       errMsg=""
+//     }
+//     setPasswordError(errMsg)
+//   }
+
+//   if(passwordInputFieldName === "confirmPassword" || (passwordInputFieldName === "password" && passwordInput.confirmPassword.length > 0)){
+
+//     if(passwordInput.confirmPassword !== passwordInput.password){
+//       setConfirmPasswordError("Confirm password is not matched")
+//     } else {
+//       setConfirmPasswordError("")
+//     }
+//   }
+// }
+
+// return(<div className="row">
+// <div className="col-sm-4">
+//   <PasswordInputField handlePasswordChange={handlePasswordChange} handlev/>
+// </div>
+// </div>)
