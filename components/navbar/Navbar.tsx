9622e3e4808ab74c11847db09fd4f3986e890203
kotltsagaan <tsgaan@gmail.com>
2023-05-02 15:22:53 +0800

    nav-agin

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,22 +1,20 @@
-import Link from "next/link";
 import NavbarItems from "./NavbarItems";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import Categories from "../menu";
 
 export default function NavBar() {
   return (
-    <div className="fixed w-full z-40">
-      <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-400 bg-opacity-90">
-        <img className="h-4 lg:h-7" src="" alt="Ecommerce pic" />
-        <div className="flex-row ml-8 gap-7 hidden lg:flex">
-          <NavbarItems label="Browse" />
-          <NavbarItems label="News" />
-          <NavbarItems label="About" />
-          <NavbarItems label="Help" />
-          <NavbarItems label="Sell" />
-          {/* <NavbarItems label={<Link href={"../auth/signin"}>Sign in</Link>} /> */}
+        <div className="px-20 md:px-16 py-2 flex flex-row justify-between items-center  bg-opacity-90">
+
+            <div className="flex-row ml-8 gap-7 hidden lg:flex justify-center w-full">
+              <Categories/>
+            {/* <NavbarItems label={<Link href={"../auth/signin"}>Sign in</Link>} /> */}
+            </div>
+            <div className="flex-row ml-8 gap-4 hidden lg:flex">
+              <FavoriteBorderOutlinedIcon/>
+              <ShoppingCartOutlinedIcon/>
+          </div>
         </div>
-      </div>
-    </div>
   );
 }
-
-
