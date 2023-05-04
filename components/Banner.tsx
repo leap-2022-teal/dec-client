b49d96e6d5fecb1f333dbf9effcaed4b1ec5f633
Evening <evening@admins-iMac-1-8.local>
2023-05-04 17:12:04 +0800

    banners

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -15,10 +15,14 @@ export function Banner() {
   const [banner, setBanner] = useState<Banner[]>([]);
   useEffect(() => {
     axios.get(`${process.env.NEXT_PUBLIC_API_URL}/banner`).then((res) => setBanner(res.data));
-  });
+  }, []);
   return (
     <>
-      <Link href={`${banner[0]?.link}`}>hi</Link>
+      <div className="">
+        <Link href={`${banner[0]?.link}`}>
+          <img src={banner[0]?.image.path} alt="image" className="w-[100%]" />
+        </Link>
+      </div>
     </>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
+import MainLayout from "@/layout/mainLayout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: any) {
   return (
     <>
-      <Component {...pageProps} />
+      <div className="container 2xl : mx-auto  max-w-[1830px]	">
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </div>
     </>
   );
 }
