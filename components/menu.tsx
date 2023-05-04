8a0112b4bac932c3aae48431e7a2cab2132591b0
Evening <evening@admins-iMac-1-8.local>
2023-05-04 14:03:57 +0800

    resolved conflicts

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,88 +1,62 @@
-import Link from "next/link";
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import axios from "axios";
 import { useState, useEffect, useRef, MutableRefObject } from "react";
-<<<<<<< HEAD
-import { Category } from "@mui/icons-material";
-=======
-import { Modal } from "react-bootstrap";
-import Search from "./search";
-import { Category, ConstructionOutlined } from "@mui/icons-material";
->>>>>>> fd90ca9 ( navbar)
 import { useOnHoverOutside } from "./hook";
 
 export default function Categories() {
-        const [categories, setCategories] = useState([]);
-        const dropdownRef = useRef(null); 
-        const [isMenuDropDownOpen, setMenuDropDownOpen] = useState("");
+  const [categories, setCategories] = useState([]);
+  const dropdownRef = useRef(null);
+  const [isMenuDropDownOpen, setMenuDropDownOpen] = useState("");
 
-        const closeHoverMenu = () => {
-            setMenuDropDownOpen("");
-          };
+  const closeHoverMenu = () => {
+    setMenuDropDownOpen("");
+  };
 
-          useOnHoverOutside(dropdownRef, closeHoverMenu);
+  useOnHoverOutside(dropdownRef, closeHoverMenu);
 
-        useEffect(() => {
-          axios
-            .get(`http://localhost:8000/categories?q`)
-            .then((res) => setCategories(res.data));
-        }, []);
+  useEffect(() => {
+    axios.get(`http://localhost:8000/categories?q`).then((res) => setCategories(res.data));
+  }, []);
 
-        return (
-                <div className="w-full" ref={dropdownRef}>
-                    <div className="flex-row gap-20 hidden lg:flex justify-center w-full">
-                        {categories.filter((category: any) => !category.parentId).map((category: any )=> (
-                         <div className="text-black-500 cursor-pointer hover:text-gray-400 transition text-l"
-                         onMouseOver={() => setMenuDropDownOpen(category._id, ) }
-                         >
-                           {category.name}
-                         </div> 
-
-<<<<<<< HEAD
-                    ))}
-                    </div>
-                        {isMenuDropDownOpen && <SubCategories isOn={isMenuDropDownOpen} categoryId={isMenuDropDownOpen} categories = {categories} />}
-
-=======
-                    ))
-                  }
-                  </div>
-                    {isMenuDropDownOpen && <SubCategories isOn={isMenuDropDownOpen} categoryId={isMenuDropDownOpen} categories = {categories} />}
->>>>>>> fd90ca9 ( navbar)
-                  </div>
-                    );
-
+  return (
+    <div className="w-full" ref={dropdownRef}>
+      <div className="flex-row gap-20 hidden lg:flex justify-center w-full">
+        {categories
+          .filter((category: any) => !category.parentId)
+          .map((category: any) => (
+            <div className="text-black-500 cursor-pointer hover:text-gray-400 transition text-l" onMouseOver={() => setMenuDropDownOpen(category._id)}>
+              {category.name}
+            </div>
+          ))}
+      </div>
+      {isMenuDropDownOpen && <SubCategories isOn={isMenuDropDownOpen} categoryId={isMenuDropDownOpen} categories={categories} />}
+    </div>
+  );
 }
 
-export function SubCategories({categories,categoryId, isOn}: any) {
-    const [subCategories, setSubCategories] = useState([]);
-    useEffect(() => {
-        if(isOn===categoryId){
-
-            const filtered = categories.filter((e:any)=>{
-                if(e.parentId===isOn){
-                    return e
-                }
-            })
-            // console.log(filtered)
-            setSubCategories(filtered)
+export function SubCategories({ categories, categoryId, isOn }: any) {
+  const [subCategories, setSubCategories] = useState([]);
+  useEffect(() => {
+    if (isOn === categoryId) {
+      const filtered = categories.filter((e: any) => {
+        if (e.parentId === isOn) {
+          return e;
         }
-        // axios
-        //   .get(`http://localhost:8000/categories`)
-        //   .then((res) => setCategories(res.data));
-      }, [isOn]);
+      });
+      // console.log(filtered)
+      setSubCategories(filtered);
+    }
+    // axios
+    //   .get(`http://localhost:8000/categories`)
+    //   .then((res) => setCategories(res.data));
+  }, [isOn]);
 
-    return (
-        <>
-            <div className="flex-row pt-10 gap-20 lg:flex justify-center w-full">
-                        {subCategories.map((category: any )=> ( 
-                         <div className="m-0 p-0 cursor-pointer hover:text-gray-300 transition text-l"
-                         >
-                           {category.name}
-                         </div> 
-                  ))}
-                    </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex-row pt-10 gap-20 lg:flex justify-center w-full">
+        {subCategories.map((category: any) => (
+          <div className="m-0 p-0 cursor-pointer hover:text-gray-300 transition text-l">{category.name}</div>
+        ))}
+      </div>
+    </>
+  );
 }
