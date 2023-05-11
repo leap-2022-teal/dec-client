b614864f6c4597e0932c48ee307cc7cd4bdd1604
Bayarkhuu13 <bayarkhuu903@gmail.com>
2023-05-11 17:17:54 +0800

    sidebar

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,3 +1,5 @@
+import { SideBar } from "@/components/Sidebar";
+import MainLayout from "@/layout/mainLayout";
 import axios from "axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -8,30 +10,35 @@ export default function AllProducts() {
     axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products?searchQuery=&categoryId=`).then((res) => setProducts(res.data));
   }, []);
   return (
-    <>
-      <section className="d-block">
-        <div className="text-left grid grid-cols-2 gap-4 laptop:grid-cols-3 ">
-          {products.map((products: any) => {
-            return (
-              <>
-                <Link href={`/products/${products._id}`}>
-                  <div className=" d-inline-block  ">
-                    <div className="products-image">
-                      <div className="aspect-[1/1] relative overflow-hidden">
-                        <img src={products.image[0].path} alt="image" />
-                      </div>
-                      <div>
-                        <h1 className="text-2xl">{products.name} </h1>
-                        <h3>${products.price}</h3>
+    <MainLayout>
+      <div className="flex ">
+        <div className="ml-15 w-80">
+          <SideBar />
+        </div>
+        <section className="d-block mt-10 ml-20">
+          <div className="text-left grid grid-cols-2 gap-4 laptop:grid-cols-3 ">
+            {products.map((products: any) => {
+              return (
+                <>
+                  <Link href={`/products/${products._id}`}>
+                    <div className=" d-inline-block  ">
+                      <div className="products-image">
+                        <div className="aspect-[1/1] relative overflow-hidden">
+                          <img src={products.image[0].path} alt="image" />
+                        </div>
+                        <div>
+                          <h1 className="text-2xl">{products.name} </h1>
+                          <h3>${products.price}</h3>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </Link>
-              </>
-            );
-          })}
-        </div>
-      </section>
-    </>
+                  </Link>
+                </>
+              );
+            })}
+          </div>
+        </section>
+      </div>
+    </MainLayout>
   );
 }
