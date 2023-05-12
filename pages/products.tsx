4c439c03b0d675ca388b9d2d4c6082a825e0a1e3
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2023-05-12 20:51:08 +0800

    clien search style next level

--- pages/products.tsx
+++ pages/products.tsx
@@ -10,35 +10,33 @@ export default function AllProducts() {
     axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products?searchQuery=&categoryId=`).then((res) => setProducts(res.data));
   }, []);
   return (
-    <MainLayout>
-      <div className="flex ">
-        <div className="ml-15 w-80">
-          <SideBar />
-        </div>
-        <section className="d-block mt-10 ml-20">
-          <div className="text-left grid grid-cols-2 gap-4 laptop:grid-cols-3 ">
-            {products.map((products: any) => {
-              return (
-                <>
-                  <Link href={`/products/${products._id}`}>
-                    <div className=" d-inline-block  ">
-                      <div className="products-image">
-                        <div className="aspect-[1/1] relative overflow-hidden">
-                          <img src={products.image[0].path} alt="image" />
-                        </div>
-                        <div>
-                          <h1 className="text-2xl">{products.name} </h1>
-                          <h3>${products.price}</h3>
-                        </div>
+    <div className="flex ">
+      <div className="ml-15 w-80">
+        <SideBar />
+      </div>
+      <section className="d-block mt-10 ml-20">
+        <div className="text-left grid grid-cols-2 gap-4 laptop:grid-cols-3 ">
+          {products.map((products: any) => {
+            return (
+              <>
+                <Link href={`/products/${products._id}`}>
+                  <div className=" d-inline-block  ">
+                    <div className="products-image">
+                      <div className="aspect-[1/1] relative overflow-hidden">
+                        <img src={products.image[0].path} alt="image" />
+                      </div>
+                      <div>
+                        <h1 className="text-2xl">{products.name} </h1>
+                        <h3>${products.price}</h3>
                       </div>
                     </div>
-                  </Link>
-                </>
-              );
-            })}
-          </div>
-        </section>
-      </div>
-    </MainLayout>
+                  </div>
+                </Link>
+              </>
+            );
+          })}
+        </div>
+      </section>
+    </div>
   );
 }
