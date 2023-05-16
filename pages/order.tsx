224d985931d049a6e01312bf622b613c9a7c9c2c
teal-2022-lhagvaerdene-mygmar <teal.2022.lhagvaerdene.mygmar@gmail.com>
2023-05-16 12:04:17 +0800

    use orders

--- pages/order.tsx
+++ pages/order.tsx
@@ -1,7 +1,7 @@
 export default function Order(productId: any) {
   return (
     <div className="container">
-      <main className="grid grid-cols-2 gap-5 max-w-[1100px] mx-auto mt-8">
+      <main className="grid grid-cols-1 laptop:grid-cols-2 gap-5 max-w-[1100px] mx-auto mt-8">
         <div>
           <h1>Bag</h1>
         </div>
