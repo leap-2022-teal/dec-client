ccaed29e541d8bb076585a015887c91c6143e939
Nyambaa0303 <115851323+Nyambaa0303@users.noreply.github.com>
2023-05-22 22:10:48 +0800

    menu blur sidebar blur menu scroll

--- components/menuDesktop.tsx
+++ components/menuDesktop.tsx
@@ -6,21 +6,37 @@ import Link from "next/link";
 import MenuMobile from "./menuMobile";
 import MenuSearch from "./menuSearch";
 
-export default function Menu() {
+export default function Menu({ toggleSearch, handleMenuDropDown, handleSideMenuOpen }: any) {
   const [menu, setMenu] = useState([]);
   const dropdownRef = useRef(null);
   const [isMenuDropDownOpen, setMenuDropDownOpen] = useState("");
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(false);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+
+    if (currentScrollPos > prevScrollPos) {
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }
+
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
 
   useEffect(() => {
     axios.get(`${process.env.NEXT_PUBLIC_API_URL}/categories?q`).then((res) => setMenu(res.data));
   }, []);
 
-  // const handleCancelSideMenu = () => {
-  //   setIsSideMenuActive(false);
-  // };
-
   const closeHoverMenu = () => {
     setMenuDropDownOpen("");
+    handleMenuDropDown(false);
   };
 
   useOnHoverOutside(dropdownRef, closeHoverMenu);
@@ -32,69 +48,77 @@ export default function Menu() {
   return (
     <>
       {/* desktop menu  */}
-      <div className={` h-16 justify-between items-center max-w-[1830px] desktop:mx-auto laptop:mx-6 mobile:hidden between:hidden tablet:hidden laptop:flex desltop:flex`}>
-        {/* nike logo image */}
-        <div className="w-24 h-16">
-          <Link href={"/"}>
-            <svg aria-hidden="true" className="pre-logo-svg w-[100%] h-[100%]" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none">
-              <path
-                fill="currentColor"
-                fill-rule="evenodd"
-                d="M21 8.719L7.836 14.303C6.74 14.768 5.818 15 5.075 15c-.836 0-1.445-.295-1.819-.884-.485-.76-.273-1.982.559-3.272.494-.754 1.122-1.446 1.734-2.108-.144.234-1.415 2.349-.025 3.345.275.2.666.298 1.147.298.386 0 .829-.063 1.316-.19L21 8.719z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </Link>
-        </div>
-
-        <div className={`w-full`} ref={dropdownRef}>
-          <div className={`${`flex-row gap-7 flex justify-center items-center h-16`}`}>
-            {menu
-              .filter((category: any) => !category.parentId)
-              .map((category: any) => (
-                <Link href={""} key={category}>
-                  {/* nemu deerhi angilaluud */}
-                  <div className="flex items-center justify-between">
-                    <div className={`${category._id === isMenuDropDownOpen ? categoryActive : categoryInActive}`} onMouseOver={() => setMenuDropDownOpen(category._id)}>
-                      {category.name}
-                    </div>
-                  </div>
-                </Link>
-              ))}
-          </div>
-          {/* subcategories  */}
-          <div className={`${isMenuDropDownOpen ? `left-[0px] absolute bg-white w-full overflow-visible justify-center flex h-[300px]  transition-all   duration-500 ease-in-out` : `h-0 `}  `}>
-            {<SubMenu isOn={isMenuDropDownOpen} categoryId={isMenuDropDownOpen} categories={menu} />}
-          </div>
-        </div>
-
-        <MenuSearch />
-        <div className="flex ">
-          {/* heart icon */}
-          <div className="desktop:flex laptop:flex mobile:hidden w-10 h-10 hover:bg-neutral-200 rounded-full  flex items-center justify-center">
-            <svg aria-hidden="true" className="pre-nav-design-icon" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none">
-              <path
-                stroke="currentColor"
-                stroke-width="1.5"
-                d="M16.794 3.75c1.324 0 2.568.516 3.504 1.451a4.96 4.96 0 010 7.008L12 20.508l-8.299-8.299a4.96 4.96 0 010-7.007A4.923 4.923 0 017.205 3.75c1.324 0 2.568.516 3.504 1.451l.76.76.531.531.53-.531.76-.76a4.926 4.926 0 013.504-1.451"
-              ></path>
-            </svg>
-          </div>
-          {/* bag icon */}
-          <div className={`w-10 h-10 hover:bg-neutral-200 rounded-full  flex items-center justify-center `}>
-            <Link href={"/order"}>
-              <svg aria-hidden="true" className="pre-nav-design-icon" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none">
+      <div className={` ${visible ? `bg-white w-full top-0 sticky` : ``}`}>
+        <div className={` h-16 justify-between items-center max-w-[1830px] desktop:mx-auto laptop:mx-6 mobile:hidden between:hidden tablet:hidden laptop:flex desltop:flex`}>
+          {/* nike logo image */}
+          <div className="w-24 h-16">
+            <Link href={"/"}>
+              <svg aria-hidden="true" className="pre-logo-svg w-[100%] h-[100%]" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none">
                 <path
-                  stroke="currentColor"
-                  stroke-width="1.5"
-                  d="M8.25 8.25V6a2.25 2.25 0 012.25-2.25h3a2.25 2.25 0 110 4.5H3.75v8.25a3.75 3.75 0 003.75 3.75h9a3.75 3.75 0 003.75-3.75V8.25H17.5"
+                  fill="currentColor"
+                  fill-rule="evenodd"
+                  d="M21 8.719L7.836 14.303C6.74 14.768 5.818 15 5.075 15c-.836 0-1.445-.295-1.819-.884-.485-.76-.273-1.982.559-3.272.494-.754 1.122-1.446 1.734-2.108-.144.234-1.415 2.349-.025 3.345.275.2.666.298 1.147.298.386 0 .829-.063 1.316-.19L21 8.719z"
+                  clip-rule="evenodd"
                 ></path>
               </svg>
             </Link>
           </div>
+
+          <div className={`w-full`} ref={dropdownRef}>
+            <div className={`${`flex-row gap-7 flex justify-center items-center h-16`}`}>
+              {menu
+                .filter((category: any) => !category.parentId)
+                .map((category: any) => (
+                  <Link href={""} key={category._id}>
+                    {/* nemu deerhi angilaluud */}
+                    <div className="flex items-center justify-between">
+                      <div
+                        className={`${category._id === isMenuDropDownOpen ? categoryActive : categoryInActive}`}
+                        onMouseOver={() => {
+                          setMenuDropDownOpen(category._id);
+                          handleMenuDropDown(true);
+                        }}
+                      >
+                        {category.name}
+                      </div>
+                    </div>
+                  </Link>
+                ))}
+            </div>
+            {/* subcategories  */}
+            <div className={`${isMenuDropDownOpen ? `left-[0px] absolute bg-white w-full overflow-visible justify-center flex h-[300px]  transition-all   duration-500 ease-in-out` : `h-0 `}  `}>
+              {<SubMenu isOn={isMenuDropDownOpen} categoryId={isMenuDropDownOpen} categories={menu} />}
+            </div>
+          </div>
+
+          <MenuSearch toggleSearch={toggleSearch} />
+          <div className="flex ">
+            {/* heart icon */}
+            <div className="desktop:flex laptop:flex mobile:hidden w-10 h-10 hover:bg-neutral-200 rounded-full  flex items-center justify-center">
+              <svg aria-hidden="true" className="pre-nav-design-icon" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none">
+                <path
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  d="M16.794 3.75c1.324 0 2.568.516 3.504 1.451a4.96 4.96 0 010 7.008L12 20.508l-8.299-8.299a4.96 4.96 0 010-7.007A4.923 4.923 0 017.205 3.75c1.324 0 2.568.516 3.504 1.451l.76.76.531.531.53-.531.76-.76a4.926 4.926 0 013.504-1.451"
+                ></path>
+              </svg>
+            </div>
+            {/* bag icon */}
+            <div className={`w-10 h-10 hover:bg-neutral-200 rounded-full  flex items-center justify-center `}>
+              <Link href={"/order"}>
+                <svg aria-hidden="true" className="pre-nav-design-icon" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none">
+                  <path
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    d="M8.25 8.25V6a2.25 2.25 0 012.25-2.25h3a2.25 2.25 0 110 4.5H3.75v8.25a3.75 3.75 0 003.75 3.75h9a3.75 3.75 0 003.75-3.75V8.25H17.5"
+                  ></path>
+                </svg>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
-      <MenuMobile categories={menu} />
+      <MenuMobile toggleSearch={toggleSearch} categories={menu} handleSideMenuOpen={handleSideMenuOpen} />
     </>
   );
 }
--- layout/mainLayout.tsx
+++ layout/mainLayout.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import NavBar from "@/components/navbar/Navbar";
 import Menu from "@/components/menuDesktop";
 import SubBanner from "@/components/subBanner";
@@ -8,6 +8,41 @@ import { useRouter } from "next/router";
 
 export const ExampleContext = createContext([]);
 export default function MainLayout({ children }: any) {
+  const [visible, setVisible] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [isMenuDropDownOpen, setMenuDropDownOpen] = useState(false);
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+
+  const toggleSearch = (e: boolean) => {
+    setIsSearchOpen(e);
+  };
+
+  const handleMenuDropDown = (e: boolean) => {
+    setMenuDropDownOpen(e);
+  };
+
+  const handleSideMenuOpen = (e: boolean) => {
+    setIsSideMenuOpen(e);
+  };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollFunction);
+    window.addEventListener("click", backToTop);
+  });
+
+  function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
   const [basket, setBasket] = useState([]);
   useEffect(() => {
     const basketItems = localStorage.getItem("basket");
@@ -16,24 +51,30 @@ export default function MainLayout({ children }: any) {
     }
   }, []);
   // const name = "bat";
-  const router = useRouter()
-  const {pathname} = router
-  console.log(pathname)
-  // if(query === "/login"){
+  const router = useRouter();
+  const { pathname } = router;
 
-  // }
+  const pageBlur = `${isSearchOpen || isMenuDropDownOpen || isSideMenuOpen ? `blur-sm relative -z-50` : ``}`;
+
   return (
     <>
       <ExampleContext.Provider value={basket}>
-        {pathname!=="/auth/signin"?
-        <>
-        <NavBar></NavBar>
-        <Menu />
-        <SubBanner />
-        </>
-        :null
-        }
-        <div className="">{children}</div>
+        {pathname !== "/auth/signin" ? (
+          <>
+            <NavBar />
+            <Menu toggleSearch={toggleSearch} handleMenuDropDown={handleMenuDropDown} handleSideMenuOpen={handleSideMenuOpen} />
+
+            <SubBanner />
+          </>
+        ) : null}
+
+        <div className={`${pageBlur}`}>
+          <div>{children}</div>
+
+          <button id="button" onClick={backToTop} className={visible ? "fixed h-10 w-10 bg-black hover:bg-neutral-500 bottom-5 right-5 rounded-full text-white" : "hidden"}>
+            <KeyboardArrowUpIcon className=" text-[30px]" />
+          </button>
+        </div>
       </ExampleContext.Provider>
     </>
   );
