fbc8580a3247f70f03ba480f486020ba0e914179
teal-2022-lhagvaerdene-mygmar <teal.2022.lhagvaerdene.mygmar@gmail.com>
2023-06-09 01:49:19 +0800

    edit env ;

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    GOOGLE_ID: "143733616857-gh4nb0vjig4u1mq2cth3uv9st0s70stk.apps.googleusercontent.com",
-    GOOGLE_SECRET: "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",
-    GITHUB_ID: "4d37752960d511071641",
-    GITHUB_SECRET: "30d498e7a1b0962e156b857041c78f72ee2d9702",
-    NEXTAUTH_URL: "http://localhost:3000/",
-    JWT_SECRET: "d7dd55296114e4c6604f86e676e91ab9",
-    NEXT_PUBLIC_API_URL: "http://localhost:8000",
-  },
+  // env: {
+  //   GOOGLE_ID: "143733616857-gh4nb0vjig4u1mq2cth3uv9st0s70stk.apps.googleusercontent.com",
+  //   GOOGLE_SECRET: "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",
+  //   GITHUB_ID: "4d37752960d511071641",
+  //   GITHUB_SECRET: "30d498e7a1b0962e156b857041c78f72ee2d9702",
+  //   NEXTAUTH_URL: "http://localhost:3000/",
+  //   JWT_SECRET: "d7dd55296114e4c6604f86e676e91ab9",
+  //   NEXT_PUBLIC_API_URL: "http://localhost:8000",
+  // },
 };
 
 module.exports = nextConfig;
